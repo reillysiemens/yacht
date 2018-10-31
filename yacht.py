e1aa7217dee9ee7150cfc088fef07cdaf899d3ae
Reilly Tucker Siemens <reilly@tuckersiemens.com>
2018-10-30 22:50:04 -0700

    Make all functions public

--- yacht.py
+++ yacht.py
@@ -23,37 +23,37 @@ class Category(Enum):
     YACHT = auto()
 
 
-def _score_ones(dice: List[int]) -> int:
+def score_ones(dice: List[int]) -> int:
     """ 1 × number of ones """
     return dice.count(1)
 
 
-def _score_twos(dice: List[int]) -> int:
+def score_twos(dice: List[int]) -> int:
     """ 2 x number of twos """
     return 2 * dice.count(2)
 
 
-def _score_threes(dice: List[int]) -> int:
+def score_threes(dice: List[int]) -> int:
     """ 3 x number of threes """
     return 3 * dice.count(3)
 
 
-def _score_fours(dice: List[int]) -> int:
+def score_fours(dice: List[int]) -> int:
     """ 4 × number of fours """
     return 4 * dice.count(4)
 
 
-def _score_fives(dice: List[int]) -> int:
+def score_fives(dice: List[int]) -> int:
     """ 5 x number of fives """
     return 5 * dice.count(5)
 
 
-def _score_sixes(dice: List[int]) -> int:
+def score_sixes(dice: List[int]) -> int:
     """ 6 x number of sixes """
     return 6 * dice.count(6)
 
 
-def _score_full_house(dice: List[int]) -> int:
+def score_full_house(dice: List[int]) -> int:
     """ Total of the dice """
     dice = sorted(dice)
     full_house = (dice[0] != dice[4] and
@@ -62,45 +62,45 @@ def _score_full_house(dice: List[int]) -> int:
     return sum(dice) if full_house else 0
 
 
-def _score_four_of_a_kind(dice: List[int]) -> int:
+def score_four_of_a_kind(dice: List[int]) -> int:
     """ Total of the four dice """
     dice = sorted(dice)
     return 4 * dice[3] if dice[0] == dice[3] or dice[1] == dice[4] else 0
 
 
-def _score_little_straight(dice: List[int]) -> int:
+def score_little_straight(dice: List[int]) -> int:
     """ 30 points """
     return 30 if sorted(dice) == LITTLE_STRAIGHT else 0
 
 
-def _score_big_straight(dice: List[int]) -> int:
+def score_big_straight(dice: List[int]) -> int:
     """ 30 points """
     return 30 if sorted(dice) == BIG_STRAIGHT else 0
 
 
-def _score_choice(dice: List[int]) -> int:
+def score_choice(dice: List[int]) -> int:
     """ Sum of the dice """
     return sum(dice)
 
 
-def _score_yacht(dice: List[int]) -> int:
+def score_yacht(dice: List[int]) -> int:
     """ 50 points """
     return 50 if all(d == dice[0] for d in dice) else 0
 
 
 _scoring: Dict[Category, Callable[[List[int]], int]] = {
-    Category.ONES: _score_ones,
-    Category.TWOS: _score_twos,
-    Category.THREES: _score_threes,
-    Category.FOURS: _score_fours,
-    Category.FIVES: _score_fives,
-    Category.SIXES: _score_sixes,
-    Category.FULL_HOUSE: _score_full_house,
-    Category.FOUR_OF_A_KIND: _score_four_of_a_kind,
-    Category.LITTLE_STRAIGHT: _score_little_straight,
-    Category.BIG_STRAIGHT: _score_big_straight,
-    Category.CHOICE: _score_choice,
-    Category.YACHT: _score_yacht,
+    Category.ONES: score_ones,
+    Category.TWOS: score_twos,
+    Category.THREES: score_threes,
+    Category.FOURS: score_fours,
+    Category.FIVES: score_fives,
+    Category.SIXES: score_sixes,
+    Category.FULL_HOUSE: score_full_house,
+    Category.FOUR_OF_A_KIND: score_four_of_a_kind,
+    Category.LITTLE_STRAIGHT: score_little_straight,
+    Category.BIG_STRAIGHT: score_big_straight,
+    Category.CHOICE: score_choice,
+    Category.YACHT: score_yacht,
 }
 
 
